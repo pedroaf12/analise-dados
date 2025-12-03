065e16e75ff2c676e87a37388c266f84e4967004
pedroaf12 <51967841+pedroaf12@users.noreply.github.com>
2025-12-03 16:25:29 -0300

    Manipulando os dados

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 import pandas as pd
+from pandasgui import show
+
+tabela = pd.read_csv("./database/ClientesBanco.csv", sep=",", encoding="latin1")
+
+# Tratando valores vazios e exibindo resumo das colunas da base de dados
+tabela = tabela.drop("CLIENTNUM", axis=1)
+tabela = tabela.dropna()
+# show(tabela.describe().round(1))
 
 
-df = pd.read_csv("./database/ClientesBanco.csv", sep=",", encoding="latin1")
-df.to_excel("ClientesBanco.xlsx", index=False)
-print(df.head())
+# Avaliando divisão entre clientes x cancelados
+qtd_categoria = tabela["Categoria"].value_counts()
+print(qtd_categoria)
+percentual = tabela["Categoria"].value_counts(normalize=True)
+print(percentual)
